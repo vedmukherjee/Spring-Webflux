2d9d9b2d5e0602490c12b4cc87d6619f33cb0cda
vedmukherjee <indrajeet.mukherjee@nashtechglobal.com>
2025-05-20 12:52:30 +0530

    webflux app

---
+++ src/main/java/com/example/demo/GreetingHandler.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+  public Mono<ServerResponse> hello(ServerRequest request) {
+    return ServerResponse.ok().bodyValue("Hello WebFlux!");
+  }
+}
